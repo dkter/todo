779537f92feefdfa238323c42cfc103f0fc25e9c
David Teresi <dkteresi+github@gmail.com>
2019-05-23 13:17:01 -0400

    Update copyright

--- lib/new_item.dart
+++ lib/new_item.dart
@@ -1,6 +1,6 @@
 /**
  * A simple to-do list app.
- * Copyright (C) David Teresi 2018
+ * Copyright (C) David Teresi 2019
  * 
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
--- lib/notify.dart
+++ lib/notify.dart
@@ -1,4 +1,7 @@
  /* 
+ * A simple to-do list app.
+ * Copyright (C) David Teresi 2019
+ * 
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
